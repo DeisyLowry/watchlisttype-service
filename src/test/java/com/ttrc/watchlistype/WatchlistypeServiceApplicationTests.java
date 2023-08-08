aea89313171b23ddd86f53cf3de853d8d5688a05
David Lowry <DavidLowry64@gmail.com>
2023-08-08 13:48:24 -0400

    Initial Commit

---
+++ src/test/java/com/ttrc/watchlistype/WatchlistypeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrc.watchlistype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistypeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
